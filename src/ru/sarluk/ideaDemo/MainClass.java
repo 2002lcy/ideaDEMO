58aa687a5be2da01d45b7a95d572ccdb0c36dc21
Sarluk <98347984>
2016-05-21 15:23:59 +0300

    Initial commit

---
+++ src/ru/sarluk/ideaDemo/MainClass.java
@@ -0,0 +1,11 @@
+package ru.sarluk.ideaDemo;
+
+/**
+ * Автор: Павел Матвеев
+ * Дата создания: 21.05.16
+ */
+public class MainClass {
+    public static void main(String[] args){
+        System.out.println("Настраиваем intelliJ IDEA для работы с github.com");
+    }
+}
